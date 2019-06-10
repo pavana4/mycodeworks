30af7d7a6e786a9265536d232d19abcc0a9bea90
pavana4 <51605642+pavana4@users.noreply.github.com>
2019-06-10 14:53:15 +0530

    NumberToWordConverterTest.java
    This is a test case file to assert the functionality of the written method

---
+++ NumberToWordConverterTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert.*;  
+import org.junit.Test;  
+  
+public class NumberToWordConverterTest {  
+  
+    @Test  
+    public void testFindMax(){  
+        assertEquals(4,Calculation.findMax(new int[]{1,3,4,2}));  
+        assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
+    }  
+}  
